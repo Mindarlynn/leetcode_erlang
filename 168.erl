631a19acf8ae6ca7516b872b51e6e403845660e1
YEGU KWON <xklest@inha.edu>
2023-11-12 06:07:52 +0900

    Update 168.erl

--- 168.erl
+++ 168.erl
@@ -1,1 +1,10 @@
-1
+-spec convert_to_title(ColumnNumber :: integer()) -> unicode:unicode_binary().
+
+convert(ColumnNumber) -> 
+    case (ColumnNumber >= 0) and (ColumnNumber < 26) of
+        true -> [$A + ColumnNumber];
+        false -> convert(ColumnNumber div 26 - 1) ++ convert(ColumnNumber rem 26)
+    end.
+
+convert_to_title(ColumnNumber) -> 
+    list_to_binary(convert(ColumnNumber - 1)).
